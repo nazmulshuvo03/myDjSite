cbcb89d756a6c4f621efe0f76d48d572fbd676bd
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-09-30 19:55:33 +0600

    more view with 404 exception

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,31 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+
     context = {
         'latest_question_list': latest_question_list
     }
-    # output = ' | '.join([q.question_text for q in latest_question_list])
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    context = {
+        'question': question
+    }
+
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
