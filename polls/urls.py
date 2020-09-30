b96def9df8cd01aaebe95350d065eb028cf63978
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-09-30 21:33:13 +0600

    generic view bug fix

--- polls/urls.py
+++ polls/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     path('<int:pk>/results/',
-         views.ResultView.as_view(), name="results"),
+         views.ResultsView.as_view(), name="results"),
     path('<int:pk>/vote/', views.vote, name="vote")
 ]
