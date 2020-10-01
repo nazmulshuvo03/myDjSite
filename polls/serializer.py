4adec59bb598ea485d67f1f0e3de0e518a4c548d
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-10-01 16:10:15 +0600

    serializer added

---
+++ polls/serializer.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import Question, Choice
+from rest_framework import serializers
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = '__all__'
+
+
